9b0b1eaf3aec98df697ace1ed342a3bb808d8713
Evaniomne <edmoura@leroymerlin.com.br>
2021-10-14 22:27:08 -0300

    refacture

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 
 const express = require('express');
-
 const cors = require('cors');
 
 require('./config/db.config')
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 
 const gameRouter = require('./routes/Game.routes')
 app.use('/', gameRouter)
