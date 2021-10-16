2b20e6eda2f3b45cacde4a7e44c17f1d03220484
Flavio-Pestana <flaviopestan@gmail.com>
2021-10-16 11:39:36 -0300

    adicionado metodo get e post

--- routes/Location.routes.js
+++ routes/Location.routes.js
@@ -3,14 +3,26 @@ const Location = require('../models/Location');
 const router = Router();
 
 //criar locaçao
-/*router.post('/', async (req, res) =>{
+router.post('/', async (req, res) =>{
     const { id } = req.user;
     try{
-        const location = await Location.create({... req.body})
+        const location = await Location.create({... req.body});
+        res.status(201).json(location);
 
     } catch(error){
-
+        res.status(500).json({message: 'Erro ao criar a locação', error})
     }
-})*/
+})
+
+//ver locaçoes
+
+router.get('/all', async (req, res) =>{
+    try{
+        const locations = await Location.find();
+        res.status(200).json(locations)
+    } catch(error){
+        res.status(500).json({ message: 'Erro ao tentar buscar todas locações', error});
+    }
+})
 
 module.exports = router;
