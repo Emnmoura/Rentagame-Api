8ae873012336182f42fa537a55fff055f3c460b9
Evaniomne <edmoura@leroymerlin.com.br>
2021-10-16 14:40:27 -0300

    Rotas Game.routes semi prontas

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
 
 const express = require('express');
+
 const cors = require('cors');
 
 require('./config/db.config')
-
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 
 const gameRouter = require('./routes/Game.routes')
 app.use('/', gameRouter)
--- routes/Game.routes.js
+++ routes/Game.routes.js
@@ -26,4 +26,44 @@ router.post('/games', async (request, response) => {
     }
 });
 
+router.get('/', async (request, response) => {
+    try {
+        const games = await game.find();
+        res.status(200).json(games);
+    } catch (error) {
+        res.status(500).json({ msg: 'Server com Error', error });
+    }
+});
+
+router.get('/:id', async (request, response) => {
+    const { id } = req.params;
+    try {
+        const game = await Game.findById(id);
+        res.status(200).json(game);
+    } catch (error) {
+        res.status(500).json({ msg: 'Erro game not found error', error });
+    }
+});
+
+router.put('/:id', async (request, response) => {
+    const { id } = request.params;
+    const payload = request.body;
+    try {
+        const updatedGame = await Game.findOneAndUpdate({ _id: id }, payload, { new: true });
+        response.status(200).json(updatedGame);
+    } catch (error) {
+        response.status(500).json({ msg: 'Game update error', error });
+    }
+});
+
+router.delete('/:id', async (request, response) => {
+    const { id } = request.params;
+    try {
+        await Game.findByIdAndDelete(id);
+        res.status(204).json();
+    } catch (error) {
+        res.status(500).json({ msg: 'Error deleting game', error });
+    }
+});
+
 module.exports = router;
