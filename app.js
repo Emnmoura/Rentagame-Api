53e8fbd1e8792e117bded803491bb9879889b818
Evaniomne <edmoura@leroymerlin.com.br>
2021-10-17 16:12:50 -0300

    user_middleware ready

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ const cors = require('cors');
 
 require('./config/db.config')
 const app = express();
+
+const userMiddleware = require('./middlewares/user.middleware');
+
 app.use(express.json());
 app.use(cors());
 
 //puxando as rotas
 const gameRouter = require('./routes/Game.routes')
 const locationRouter = require('./routes/Location.routes')
+
+//Autenticação
+app.use(userMiddleware);
 
 //rotas
 app.use('/', gameRouter)
